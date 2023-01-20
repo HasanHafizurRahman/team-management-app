e1fffdc14a0bb8d2749d273d4021321eaa017244
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-01-21 00:46:50 +0600

    bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Team Manager</span>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
